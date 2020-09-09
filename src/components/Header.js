f1680184b669bf8678aab35a8f648815f00a4b11
Vadim Martin <fragariastar@gmail.com>
2020-09-09 21:08:52 +0400

    added search box

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Header.css';
 import logo from '../images/amazon_logo.png';
 import { Link } from 'react-router-dom';
+import SearchIcon from '@material-ui/icons/Search';
 
 function Header() {
     return (
@@ -11,7 +12,10 @@ function Header() {
             <img className="header__logo" src={logo} alt='' /> 
             </Link>
             {/* search-box */}
-            <input className='header__searchInput' type='text' />
+            <div className='header__search'>
+               <input className='header__searchInput' type='text' />
+               <SearchIcon  className="header__searchIcon"/>
+            </div>
             {/* 3 links  */}
             {/* basket icon and number*/}
         </nav>
