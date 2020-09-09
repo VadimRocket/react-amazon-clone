fcddc3c472726bd6a73170396fa205530dc89029
Vadim Martin <fragariastar@gmail.com>
2020-09-09 20:22:19 +0400

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      {/* home, login, checkout */}
     </div>
   );
 }
