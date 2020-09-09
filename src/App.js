921f714f2ff0b0491422c264f7f9bbf50484c05b
Vadim Martin <fragariastar@gmail.com>
2020-09-09 20:25:51 +0400

    added routes

--- src/App.js
+++ src/App.js
@@ -4,9 +4,22 @@ import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="app">
+    <Router>
+      <div className="app">
       {/* home, login, checkout */}
+      <Switch>
+        <Route path="/">
+          <h2>Home page</h2>
+        </Route>
+        <Route path="/login">
+          <h2>login page</h2>
+        </Route>
+        <Route path="/checkout">
+          <h2>checkout page</h2>
+        </Route>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
